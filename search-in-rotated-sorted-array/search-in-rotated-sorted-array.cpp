1c7b050537c9b1f57390b23409ba6b3d475e3189
codingkick <57174101+codingkick@users.noreply.github.com>
2021-10-23 17:22:20 +0530

    Time: 7 ms (30.21%), Space: 11.2 MB (29.91%) - LeetHub

--- search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -5,37 +5,44 @@ public:
         int l = 0,r = n-1;
         while(l<=r)
         {
+            int val = nums[l];
+            while(l<=r)
+            {
+                if(nums[l]==val)
+                    l++;
+                else
+                    break;
+            }
+            l--;
+            val = nums[r];
+            while(l<=r)
+            {
+                if(nums[r] == val)
+                    r--;
+                else
+                    break;
+            }
+            r++;
             int mid = (l+r)/2;
-            // int v1 = nums[l];
-            // int v2 = nums[mid];
-            // int v3 = nums[r];
             if(nums[mid] == target)
                 return mid;
             else
             {
-                if(nums[l]<=nums[mid])
+                if(nums[mid]>=nums[l])
                 {
-//                     homo part is l to mid
-                    if(nums[l]<=target && nums[mid]>target)
-                    {
+//                     l to mid sorted
+                    if(target>=nums[l] && target<nums[mid])
                         r = mid-1;
-                    }
                     else
-                    {
-                        l = mid + 1;
-                    }
+                        l = mid+1;
                 }
                 else
                 {
-                    // homo part is mid to r
-                    if(nums[mid] < target && nums[r]>=target)
-                    {
-                        l = mid + 1;
-                    }
+                    // mid to r sorted
+                    if(target>nums[mid] && target<=nums[r])
+                        l = mid+1;
                     else
-                    {
-                        r = mid - 1;
-                    }
+                        r = mid-1;
                 }
             }
         }
