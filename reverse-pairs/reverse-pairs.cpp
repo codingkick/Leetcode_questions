53de0aa5d557e7e7ef06d25b8b012b0229211ace
codingkick <57174101+codingkick@users.noreply.github.com>
2021-10-23 19:24:36 +0530

    Time: 1061 ms (19.34%), Space: 237.4 MB (24.09%) - LeetHub

--- reverse-pairs/reverse-pairs.cpp
+++ reverse-pairs/reverse-pairs.cpp
@@ -3,64 +3,72 @@ public:
     void merge(vector<int> &nums,int l,int mid,int r)
     {
         vector<int> arr;
-        int i = l,j = mid + 1;
-        while(i<=mid && j<=r)
+        int p=l,q=mid+1;
+        while(p<=mid && q<=r)
         {
-            if(nums[i]<=nums[j])
+            if(nums[p]<=nums[q])
             {
-                arr.push_back(nums[i]);
-                i++;
+                arr.push_back(nums[p]);
+                p++;
             }
             else
             {
-                arr.push_back(nums[j]);
-                j++;
+                arr.push_back(nums[q]);
+                q++;
             }
         }
-        while(i<=mid)
+        while(p<=mid)
         {
-            arr.push_back(nums[i]);
-            i++;
+            arr.push_back(nums[p]);
+            p++;
         }
-        while(j<=r)
+        while(q<=r)
         {
-            arr.push_back(nums[j]);
-            j++;
+            arr.push_back(nums[q]);
+            q++;
         }
-        int m = 0;
-        for(int k=l;k<=r;k++)
+        int k = 0;
+        for(int i=l;i<=r;i++)
         {
-            nums[k] = arr[m];
-            m++;
+            nums[i] = arr[k];
+            k++;
         }
     }
     int mergesort(vector<int> &nums,int l,int r)
     {
         if(l>=r)
             return 0;
-        else
+        int mid = (l+r)/2;
+        int count = 0;
+        count += mergesort(nums,l,mid);
+        count += mergesort(nums,mid+1,r);
+        int i=l,j=mid+1;
+        int val = 0;
+        while(i<=mid)
         {
-            int count = 0;
-            int mid = l + (r-l)/2;
-            count += mergesort(nums,l,mid);
-            count += mergesort(nums,mid+1,r);
-            int i = l,j=mid+1;
-            while(i<=mid)
+            while(j<=r)
             {
-                while(j<=r && nums[i]/2.0 > nums[j])
+                if((double)nums[i]/2.0 > (double)nums[j])
                 {
                     j++;
                 }
-                i++;
-                count += (j-mid-1);
+                else
+                    break;
             }
-            // merge(nums,l,mid,r);
-            stable_sort(nums.begin() + l,nums.begin() + r+1);
-            return count;
+            count += (j-(mid+1)+1)-1;
+            val += (j-(mid+1)+1)-1;
+            i++;
         }
+        // cout<<val<<"\n";
+        merge(nums,l,mid,r);
+        return count;
     }
     int reversePairs(vector<int>& nums) {
         int n = nums.size();
-        return mergesort(nums,0,n-1);
+        int l = 0,r = n-1;
+        int ans = mergesort(nums,0,n-1);
+        // for(auto it:nums)
+        //     cout<<it<<" ";
+        return ans;
     }
 };
