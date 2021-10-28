acf521ebfc6886581b81d19d3bae931ddf464ed5
codingkick <57174101+codingkick@users.noreply.github.com>
2021-10-28 10:11:50 +0530

    Time: 52 ms (98.41%), Space: 19.9 MB (96.45%) - LeetHub

--- 3sum/3sum.cpp
+++ 3sum/3sum.cpp
@@ -1,34 +1,39 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
         int n = nums.size();
-        int i = 0;
-        int target = 0;
+        sort(nums.begin(),nums.end());
         vector<vector<int>> ans;
+        int i = 0;
         while(i<n)
         {
             int v1 = nums[i];
             int p = i+1,q = n-1;
             while(p<q)
             {
-                long long int val = (long long int)nums[i] + (long long int)nums[p] + (long long int)nums[q];
-                int v2 = nums[p],v3 = nums[q];
-                if(val == target)
+                int val = v1 + nums[p] + nums[q];
+                if(val>0)
                 {
-                    ans.push_back({v1,v2,v3});
+                    q--;
+                }
+                else if(val<0)
+                {
+                    p++;
+                }
+                else
+                {
+                    ans.push_back({v1,nums[p],nums[q]});
+                    int v2 = nums[p],v3 = nums[q];
                     while(p<q && nums[p] == v2)
                         p++;
-                    while(p<q && nums[q] == v3)
+                    while(p>q && nums[q] == v3)
                         q--;
                 }
-                else if(val > target)
-                    q--;
-                else
-                    p++;
             }
             while(i<n && nums[i] == v1)
+            { 
                 i++;
+            }
         }
         return ans;
     }
