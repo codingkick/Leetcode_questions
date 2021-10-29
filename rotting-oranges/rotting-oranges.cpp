f40a9bbadd27378196e205ce7cca5e645cab87a5
codingkick <57174101+codingkick@users.noreply.github.com>
2021-10-29 11:24:25 +0530

    Time: 4 ms (92.11%), Space: 14 MB (12.52%) - LeetHub

--- rotting-oranges/rotting-oranges.cpp
+++ rotting-oranges/rotting-oranges.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int bfs(vector<vector<int>>& grid)
+    int bfs(vector<vector<int>>& grid,vector<vector<int>>& vis)
     {
         int m = grid.size();
         int n = grid[0].size();
@@ -12,50 +12,46 @@ public:
                 if(grid[i][j] == 2)
                 {
                     q1.push({i,j});
+                    vis[i][j] = 1;
                 }
             }
         }
-        int time = 0;
+        int time = -1;
         while(!q1.empty())
         {
+            time++;
             int s = q1.size();
             while(s--)
             {
-                vector<int> temp;
-                temp = q1.front();
+                vector<int> temp = q1.front();
                 q1.pop();
                 int x = temp[0],y = temp[1];
-                if(x-1>=0 && grid[x-1][y] == 1)
+                if(x-1>=0 && grid[x-1][y] == 1 && vis[x-1][y] == 0)
                 {
-                    grid[x-1][y] = 2;
                     q1.push({x-1,y});
+                    vis[x-1][y] = 1;
+                    grid[x-1][y] = 2;
                 }
-                if(y-1>=0 && grid[x][y-1] == 1)
+                if(x+1<m && grid[x+1][y] == 1 && vis[x+1][y] == 0)
                 {
-                    grid[x][y-1] = 2;
-                    q1.push({x,y-1});
-                }
-                if(x+1<m && grid[x+1][y] == 1)
-                {
-                    grid[x+1][y] = 2;
                     q1.push({x+1,y});
+                    vis[x+1][y] = 1;
+                    grid[x+1][y] = 2;
                 }
-                if(y+1<n && grid[x][y+1] == 1)
+                if(y-1>=0 && grid[x][y-1] == 1 && vis[x][y-1] == 0)
                 {
-                    grid[x][y+1] = 2;
+                    q1.push({x,y-1});
+                    vis[x][y-1] = 1;
+                    grid[x][y-1] = 2;
+                }
+                if(y+1<n && grid[x][y+1] == 1 && vis[x][y+1] == 0)
+                {
                     q1.push({x,y+1});
+                    vis[x][y+1] = 1;
+                    grid[x][y+1] = 2;
                 }
             }
-            time++;
         }
-        if(time != 0)
-            return time-1;
-        return time;
-    }
-    int orangesRotting(vector<vector<int>>& grid) {
-        int ans = bfs(grid);
-        int m = grid.size();
-        int n = grid[0].size();
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
@@ -66,6 +62,16 @@ public:
                 }
             }
         }
+        if(time == -1)
+            return 0;
+        return time;
+    }
+    int orangesRotting(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<int> vec(n,0);
+        vector<vector<int>> vis(m,vec);
+        int ans = bfs(grid,vis);
         return ans;
     }
 };
