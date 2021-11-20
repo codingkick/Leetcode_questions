d523f2edcc7d984a77cbac7b87ea9479d53fcb38
codingkick <57174101+codingkick@users.noreply.github.com>
2021-11-20 11:27:51 +0530

    Time: 8 ms (77.96%), Space: 11.1 MB (38.82%) - LeetHub

--- single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -5,25 +5,29 @@ public:
         int l = 0,r = n-1;
         while(l<=r)
         {
-            int mid = l + (r-l)/2;
-            
-            if(mid!=0 && nums[mid] == nums[mid-1])
-            {
-                if((mid+1-2)%2 == 0)
-                    l = mid+1;
-                else
-                    r = mid-1;
-            }
-            else if(mid!=n-1 && nums[mid] == nums[mid+1])
-            {
-                if((mid+1-1)%2 == 0)
-                    l = mid+1;
-                else
-                    r = mid-1;
-            }
-            else
+            int mid = (l+r)/2;
+            if((mid==0 || nums[mid]!=nums[mid-1]) && (mid==n-1 || nums[mid]!=nums[mid+1]))
                 return nums[mid];
+            else
+            {
+                if(mid!=0 && nums[mid]==nums[mid-1])
+                {
+                    int count = mid-1;
+                    if(count%2 == 0)
+                        l = mid+1;
+                    else
+                        r = mid-1;
+                }
+                else
+                {
+                    int count = mid;
+                    if(count%2 == 0)
+                        l = mid+1;
+                    else
+                        r = mid-1;
+                }
+            }
         }
-        return 0;
+        return nums[0];
     }
 };
