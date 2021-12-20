d1491b2376e067094984fde94d0d9f891d793583
codingkick <57174101+codingkick@users.noreply.github.com>
2021-12-20 18:32:23 +0530

    Time: 4 ms (97.66%), Space: 9.1 MB (16.19%) - LeetHub

--- edit-distance/edit-distance.cpp
+++ edit-distance/edit-distance.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int func(string w1,string w2,int i,int j,vector<vector<int>> &mem)
+    int func(string &word1,string &word2,int i,int j,vector<vector<int>> &mem)
     {
         if(i == 0)
             return j;
@@ -10,23 +10,21 @@ public:
             return mem[i][j];
         else
         {
-            if(w1[i-1] == w2[j-1])
-            {
-                mem[i][j] = func(w1,w2,i-1,j-1,mem);
-            }
+            if(word2[i-1] == word1[j-1])
+                mem[i][j] = func(word1,word2,i-1,j-1,mem);
             else
             {
-                int v1,v2,v3;
-                v1 = func(w1,w2,i,j-1,mem);
-                v2 = func(w1,w2,i-1,j,mem);
-                v3 = func(w1,w2,i-1,j-1,mem);
-                mem[i][j] = 1 + min({v1,v2,v3});
+                int ins = func(word1,word2,i,j-1,mem);
+                int del = func(word1,word2,i-1,j,mem);
+                int rep = func(word1,word2,i-1,j-1,mem);
+                mem[i][j] = 1 + min(min(ins,del),rep);
             }
             return mem[i][j];
         }
     }
     int minDistance(string word1, string word2) {
-        int n = word1.length(),m = word2.length();
+        int m = word1.length();
+        int n = word2.length();
         vector<int> vec(m+1,-1);
         vector<vector<int>> mem(n+1,vec);
         return func(word1,word2,n,m,mem);
