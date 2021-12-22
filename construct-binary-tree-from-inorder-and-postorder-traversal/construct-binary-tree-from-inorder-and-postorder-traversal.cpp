0e0d86b6d7afec4eac28243198e6fa1ebbf52f36
codingkick <57174101+codingkick@users.noreply.github.com>
2021-12-22 16:59:05 +0530

    Time: 16 ms (75.80%), Space: 26.4 MB (49.96%) - LeetHub

--- construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -11,27 +11,24 @@
  */
 class Solution {
 public:
-    pair<TreeNode*,int> build(vector<int>& in, vector<int>& post,int postind,unordered_map<int,int> &mpp,int l,int r)
+    int postind;
+    TreeNode* tree(vector<int> &in,vector<int> &post,int l,int r,unordered_map<int,int> &mpp)
     {
-        if(postind<0)
-            return {NULL,postind};
         if(l>r)
-            return {NULL,postind};
-        TreeNode *new_node = new TreeNode();
-        new_node->val = post[postind];
+            return NULL;
+        TreeNode *node = new TreeNode();
+        node->val = post[postind];
         postind--;
-        int ind = mpp[new_node->val];
-        pair<TreeNode*,int> v1 = build(in,post,postind,mpp,ind+1,r);
-        new_node->right = v1.first;
-        pair<TreeNode*,int> v2 = build(in,post,v1.second,mpp,l,ind-1);
-        new_node->left = v2.first;
-        return {new_node,v2.second};
+        node->right = tree(in,post,mpp[node->val]+1,r,mpp);
+        node->left = tree(in,post,l,mpp[node->val]-1,mpp);
+        return node;
     }
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
         int n = inorder.size();
         unordered_map<int,int> mpp;
         for(int i=0;i<n;i++)
             mpp[inorder[i]] = i;
-        return build(inorder,postorder,n-1,mpp,0,n-1).first;
+        postind = n-1;
+        return tree(inorder,postorder,0,n-1,mpp);
     }
 };
