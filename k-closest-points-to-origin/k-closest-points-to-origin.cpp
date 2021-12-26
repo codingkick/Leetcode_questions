b3d65ce2e19a7143166d0a313bc348c0ff09c341
codingkick <57174101+codingkick@users.noreply.github.com>
2021-12-26 12:40:48 +0530

    Time: 244 ms (36.16%), Space: 67.9 MB (21.81%) - LeetHub

--- k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        priority_queue<vector<int>> pq;
         int n = points.size();
-        for(int i=0;i<k;i++)
+        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>> pq;
+        for(int i=0;i<n;i++)
         {
-            int dis = (points[i][0])*(points[i][0]) + (points[i][1])*(points[i][1]);
-            pq.push({dis,points[i][0],points[i][1]});
-        }
-        for(int i=k;i<n;i++)
-        {
-            int dis = (points[i][0])*(points[i][0]) + (points[i][1])*(points[i][1]);
-            vector<int> temp = pq.top();
-            // cout<<temp[0];
-            if(temp[0]>dis)
-            {
-                pq.pop();
-                pq.push({dis,points[i][0],points[i][1]});
-            }
+            int x = points[i][0];
+            int y = points[i][1];
+            int val = x*x + y*y;
+            pq.push({val,x,y});
         }
         vector<vector<int>> ans;
-        while(!pq.empty())
+        while(k--)
         {
             vector<int> temp = pq.top();
             pq.pop();
